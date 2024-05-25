d34a85c1381ec71cb9fd3719918ebdb93415533d
Bruna de Moura <brunninhademoura@gmail.com>
2024-05-25 00:38:56 -0300

    Added files that was last edited in May-12-2022.

--- server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -1,22 +1,22 @@
-import { MailAdapter, SendMailData } from "../mail-adapter";
-import nodemailer from 'nodemailer';
+// import { MailAdapter, SendMailData } from "../mail-adapter";
+// import nodemailer from 'nodemailer';
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "1bb2e901d3f91b",
-      pass: "461847c2216d3e"
-    }
-  });
+// const transport = nodemailer.createTransport({
+//     host: "smtp.mailtrap.io",
+//     port: 2525,
+//     auth: {
+//       user: "1bb2e901d3f91b",
+//       pass: "461847c2216d3e"
+//     }
+//   });
 
-export class NodemailerMailAdapter implements MailAdapter {
-    async sendMail({subject, body}: SendMailData) {
-        await transport.sendMail({
-            from: 'Equipe Feedget <oi@feedget.com>',
-            to: 'Bruna Moura <brunninhademoura@gmail.com>',
-            subject,
-            html: body,
-        })
-    }
-}
+// export class NodemailerMailAdapter implements MailAdapter {
+//     async sendMail({subject, body}: SendMailData) {
+//         await transport.sendMail({
+//             from: 'Equipe Feedget <oi@feedget.com>',
+//             to: 'Bruna Moura <brunninhademoura@gmail.com>',
+//             subject,
+//             html: body,
+//         })
+//     }
+// }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import cors from 'cors';
-import { routes } from './routes';
+// //import cors from 'cors';
+// import { routes } from './routes';
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
+// //app.use(cors());
+// app.use(express.json());
+// app.use(routes);
 
-
+app.post('/feedbacks', (req, res) => {
+    return res.send('Agora vai')
+})
 
 app.listen(3333, () => {
-    console.log('está funcionando o servidor')
+    console.log('HTTP server running')
 });
